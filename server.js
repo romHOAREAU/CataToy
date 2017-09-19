8f67c20d7bd98edd4913ff1db22f801463900fc4
Romain <romhoareaupro@gmail.com>
2017-09-19 22:24:02 +0200

    Ajout du jouet via le formulaire en POST

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
 const Jouet = require('./jouet.js');
 const TrancheAge = require('./trancheAge.js');
 const Categorie = require('./categorie.js');
 
-// DONNEES
-let tranche02 =  new TrancheAge("tranche02",0,2);
-let tranche25 =  new TrancheAge("tranche25",2,5);
-let tranche48 =  new TrancheAge("tranche48",4,8);
+const app = express();
 
-let categorie1 = new Categorie("Plein Air");
-let categorie2 = new Categorie("Jeux d'imagination");
-let categorie3 = new Categorie("Jeux d'eveil");
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+// DONNEES
+let lesTranches=[];
+let tranche02 =  new TrancheAge(1,"tranche02",0,2);
+lesTranches.push(tranche02);
+let tranche25 =  new TrancheAge(2,"tranche25",2,5);
+lesTranches.push(tranche25);
+let tranche48 =  new TrancheAge(3,"tranche48",4,8);
+lesTranches.push(tranche48);
+
+let lesCateg =[];
+let categorie1 = new Categorie(1,"Plein Air");
+lesCateg.push(categorie1);
+let categorie2 = new Categorie(2,"Jeux d'imagination");
+lesCateg.push(categorie2);
+let categorie3 = new Categorie(3,"Jeux d'eveil");
+lesCateg.push(categorie3);
 
 let lesJouets = [];
 let unJouet1 = new Jouet(1,"Draisienne junior bleue",tranche25,categorie1);
@@ -35,50 +49,62 @@ app.get('/', function (req, res) {
 //affiche tous les jouets
 app.get('/jouets',
     (req,res) =>  {
-    let responseText = 'Voici la liste des jouets </br>';
-lesJouets.forEach(
-    (unJouet) => {responseText += `Jouet : ${unJouet.libelle}  </br>`;}
-            );
+        let responseText = 'Voici la liste des jouets </br>';
+        lesJouets.forEach(
+            (unJouet) => {responseText += `Jouet : ${unJouet.libelle}  </br>`;}
+                );
         responseText += '<hr>';
         responseText += 'Ajout d\'un nouveau jouet </br>';
         responseText += '<form action="/jouets" method="post">';
         responseText += 'Libelle :<br>';
         responseText += '<input type="text" name="libelle" value=""><br>';
         responseText += 'Categorie :<br>';
-        responseText += '<select>';
-        responseText += `<option value="cat1"> ${categorie1.libelle}</option>`;
-        responseText += `<option value="cat2"> ${categorie2.libelle}</option>`;
-        responseText += `<option value="cat3"> ${categorie3.libelle}</option>`;
+        responseText += '<select name="categ">';
+        responseText += `<option value="1"> ${categorie1.libelle}</option>`;
+        responseText += `<option value="2"> ${categorie2.libelle}</option>`;
+        responseText += `<option value="3"> ${categorie3.libelle}</option>`;
         responseText +='</select><br>';
         responseText += 'Tranche d\'âge :<br>';
-        responseText += '<select>';
-        responseText += `<option value="t02"> ${tranche02.toString()}</option>`;
-        responseText += `<option value="t25"> ${tranche25.toString()}</option>`;
-        responseText += `<option value="t48"> ${tranche48.toString()}</option>`;
+        responseText += '<select name="tranche">';
+        responseText += `<option value="1"> ${tranche02.toString()}</option>`;
+        responseText += `<option value="2"> ${tranche25.toString()}</option>`;
+        responseText += `<option value="3"> ${tranche48.toString()}</option>`;
         responseText +='</select><br>';
         responseText += '<input type="submit" value="Envoyer">';
         responseText += '</form>';
 
         res.send(responseText);
 
-    });
+});
+
+//Ajoute un jouet
+app.post('/jouets', (req, res) => {
+    let jouetLib = req.body.libelle;
+    let categ  = req.body.categ;
+    let trancheA = req.body.tranche;
+
+    lesJouets.push(new Jouet(lesJouets.length+1,jouetLib,lesTranches[trancheA-1],lesCateg[categ-1]));
+    console.log("Jouet ajouté");
+    res.redirect('/jouets');
+
+
+});
 
 //Affiche le jouet d'id :id
 app.get('/jouets/:id',
     (req, res) => {
 
-    let id=req.params.id;
+        let id=req.params.id;
 
-if (id < lesJouets.length+1){
-    let responseText = `Jouet : ${lesJouets[id-1].libelle}`;
-    res.send(responseText);
-}
-else
-{
-    res.status(404).send('Sorry ! Toy doesn\'t exist');
-}
+        if (id < lesJouets.length+1){
+            let responseText = `Jouet : ${lesJouets[id-1].libelle}`;
+            res.send(responseText);
+        }
+        else
+        {
+            res.status(404).send('Sorry ! Toy doesn\'t exist');
+        }
 });
-
 
 
 
@@ -90,7 +116,7 @@ else
 try {
     app.listen(8081,'127.0.0.1',() => {
         console.log("Serveur demarré");
-});
+    });
 } catch (err) {
     console.error(' Erreur de démarrage du serveur!', err);
 }
