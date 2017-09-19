d769e628895d16aa6408c73cb9608486f3ea6dd2
HOAREAU <romhoareaupro@gmail.com>
2017-09-19 11:55:22 +0200

    Creation de la classe categorie (part2)

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const Jouet = require('./jouet.js');
 const TrancheAge = require('./trancheAge.js');
+const Categorie = require('./categorie');
 
 console.log("Bienvenue dans le catalogue de jouet");
 
 let tranche25 =  new TrancheAge("tranche25",2,5);
 console.log(tranche25.toString());
+
+let categorie1 = new Categorie("Plein Air");
+console.log(categorie1.libelle);
 
 let unJouet = new Jouet("Draisienne junior bleue",tranche25);
 console.log(unJouet.trancheAge);
