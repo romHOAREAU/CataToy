b93c600f4278aa8d36c6877cb97a52c5bf0e43c4
HOAREAU <romhoareaupro@gmail.com>
2017-09-19 11:47:54 +0200

    Affectation d'une tranche d'age à un jouet

--- jouet.js
+++ jouet.js
@@ -1,12 +1,19 @@
 class Jouet {
 
-    constructor(unLibelle){
+
+    constructor(unLibelle,uneTancheAge){
         this._libelle = unLibelle;
+        this._trancheAge = uneTancheAge;
     }
 
     get libelle(){
         return this._libelle;
     }
+
+    get trancheAge(){
+        return this._trancheAge.toString();
+    }
+
 
 }
 module.exports = Jouet;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const TrancheAge = require('./trancheAge.js');
 
 console.log("Bienvenue dans le catalogue de jouet");
 
-let unJouet = new Jouet("Draisienne junior bleue");
-console.log(unJouet.libelle);
-
 let tranche25 =  new TrancheAge("tranche25",2,5);
 console.log(tranche25.toString());
+
+let unJouet = new Jouet("Draisienne junior bleue",tranche25);
+console.log(unJouet.trancheAge);
