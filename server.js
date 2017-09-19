61c2c561293ff41fd4c4b60c7e41da731a0d84d6
Romain <romhoareaupro@gmail.com>
2017-09-19 21:13:07 +0200

    Transformation en application web express

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-const Jouet = require('./jouet.js');
-const TrancheAge = require('./trancheAge.js');
-const Categorie = require('./categorie');
+const express = require('express');
+const app = express();
 
-console.log("Bienvenue dans le catalogue de jouet");
+app.get('/', function (req, res) {
+    res.send('Bienvenue dans le catalogue de jouet');
+});
 
-let tranche25 =  new TrancheAge("tranche25",2,5);
-//console.log(tranche25.toString());
-
-let categorie1 = new Categorie("Plein Air");
-//console.log(categorie1.libelle);
-
-let unJouet = new Jouet("Draisienne junior bleue",tranche25,categorie1);
-//console.log(unJouet.trancheAge);
-
-categorie1.afficherJouets(console.log);
+app.listen(8081);
