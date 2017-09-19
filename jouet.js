0575de69db9ad68366c2da225f27f8c240b26784
HOAREAU <romhoareaupro@gmail.com>
2017-09-19 11:36:57 +0200

    Creation de la classe Jouet avec un attribut libelle

---
+++ jouet.js
@@ -0,0 +1,12 @@
+class Jouet {
+    constructor(libelle){
+                this._libelle = libelle;
+            }
+
+
+    get libelle(){
+                return this._libelle;
+            }
+
+}
+module.exports = Jouet;
--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const Jouet = require('./jouet.js');
+
+console.log("Bienvenue dans le catalogue de jouet");
+
+var unJouet = new Jouet("Draisienne junior bleue");
+
+console.log(unJouet.libelle);
